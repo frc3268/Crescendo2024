1aa2183bd23e5060fcb4817739b82aee06c8ddc9
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2024-10-07 16:07:09 -0700

    Removed Duplicated files

--- src/main/kotlin/frc/robot/subsystems/intake/IntakeIOSim.kt
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.intake
-
-class IntakeIOSim() : IntakeIO {
-
-    //override all the methods in IntakeIO
-}
--- src/main/kotlin/frc/robot/subsystems/intake/IntakeIOSparkMax.kt
+++
@@ -1,4 +0,0 @@
-package frc.robot.subsystems.intake
-
-class IntakeIOSparkMax():IntakeIO {
-}
